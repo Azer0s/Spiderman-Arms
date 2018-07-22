89f6a526ba96b6f3d1c2d0986a02da8a809bd27a
Unknown <sim17057@spengergasse.at>
2018-07-22 22:12:09 +0200

    Added Arduino program

---
+++ SpidermanArms.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+Servo motor;
+const int buttonPin = 7; 
+bool toggle = false;
+int pos = 0;
+
+void setup() {
+	pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+	motor.attach(4,750,2245);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == HIGH) {
+    delay(500);
+    if(toggle){
+        Serial.println("turn");
+        for (pos = 0; pos <= 180; pos += 1) {
+          motor.write(pos);
+          delay(1);
+        }
+    }else{
+      Serial.println("rev");
+      for (pos = 180; pos >= 0; pos -= 1) {
+        motor.write(pos);
+        delay(1); 
+      }
+    }
+    
+    Serial.println(pos);
+    toggle = !toggle;
+  }
+}
